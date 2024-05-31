3c4f417fe17fd5ce74bfecfb37834a496aeeb7df
rafael <rafael.florianoc9@gmail.com>
2024-05-31 18:16:01 -0300

    BUILD: implemented 'pringdoc-openapi-starter-webmvc-ui' in my project

---
+++ src/main/java/br/com/rafael/floriano/spring_with_swagger_implementation/SpringWithSwaggerImplementationApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.floriano.spring_with_swagger_implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithSwaggerImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithSwaggerImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafael/floriano/spring_with_swagger_implementation/SpringWithSwaggerImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.floriano.spring_with_swagger_implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithSwaggerImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
