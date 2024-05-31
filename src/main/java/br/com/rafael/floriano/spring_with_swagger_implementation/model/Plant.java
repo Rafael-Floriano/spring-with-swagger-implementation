606b8740207223a7e8095f3673023cb0859f6681
rafael <rafael.florianoc9@gmail.com>
2024-05-31 18:16:39 -0300

    FEAT: create new model 'Plant'

---
+++ src/main/java/br/com/rafael/floriano/spring_with_swagger_implementation/model/Plant.java
@@ -0,0 +1,21 @@
+package br.com.rafael.floriano.spring_with_swagger_implementation.model;
+
+import java.io.Serializable;
+
+
+public class Plant implements Serializable {
+
+    private String name;
+
+    public Plant(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
